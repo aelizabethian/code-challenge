dee47b14e9ba329f41f2c482b9ebfc82a5e56c74
aelizabethian <89468695+aelizabethian@users.noreply.github.com>
2021-09-23 11:30:08 -0400

    Update submission.js

--- submission.js
+++ submission.js
@@ -1,23 +1,110 @@
 const findSum = function(array) {
-  // your code here - don't forget to return a number!
+   let total=0;
+  for(let num of array) {
+      total+= num;
+  } return total;
 };
 
 const findFrequency = function(array) {
-  // your code here - don't forget to return an object!
+   let obj = {};
+    let mostNo=-Infinity;
+    let leastNo = Infinity;
+    let mostFreq = "";
+    let leastFreq= "";
+
+
+    for(let el of array) {
+        obj[el] = obj[el] + 1 || 1;
+    }
+
+    for(let el in obj) {
+        if(obj[el] > mostNo) {
+            mostNo = obj[el];
+            mostFreq = el; 
+        }
+    }
+
+
+   for(let el in obj) {
+        if(obj[el] < leastNo) {
+            leastNo = obj[el];
+            leastFreq = el; 
+        }
+    }
+
+    return {most: mostFreq, least: leastFreq};
+
 };
+
+
 
 const isPalindrome = function(str) {
-  // your code here - don't forget to return a boolean!
+     let str2 = str;
+     const newVar = str.split('').reverse('').join('');
+
+    if(str2.toLowerCase() === newVar.toLowerCase()) {
+        return true;
+    }
+       
+      return false;
 };
+
+
 
 const largestPair = function(array) {
-  // your code here - don't forget to return a number!
+  let product = -Infinity;
+
+    for(let i=0; i<array.length; i++) {
+        if ((array[i] * array[i+1]) > product) {
+            product = (array[i] * array[i+1]);
+        }
+    } return product;
+ 
 };
 
+
 const removeParenth = function(str) {
-  // your code here - don't forget to return a string!
+   str=str.replace(/[()]/g,"");
+            return str;
 };
 
 const scoreScrabble = function(str) {
-  // your code here - don't forget to return a number!
+   let total = 0;
+
+    for(let letter of str.toLowerCase()) {
+        
+    switch(true) {
+        case 'aeioulnrst'.includes(letter):
+        total+=1;
+        break;
+         
+         case 'dg'.includes(letter):
+        total+=2;
+        break; 
+
+        case 'bcmp'.includes(letter):
+        total+=3;
+        break; 
+
+        case 'fhvwy'.includes(letter):
+        total+=4;
+        break; 
+
+        case 'k'.includes(letter):
+        total+=5;
+        break;
+        
+        case 'jx'.includes(letter):
+        total+=8;
+        break;
+
+        case 'qz'.includes(letter):
+        total+=10;
+        break;
+    
+        default: 0;
+
+    }
+
+} return total;
 };
